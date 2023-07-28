1d2598bdcb462f1950ab21216f7cd70a33025e95
Makgwale Moila <makgwalemoila@gmail.com>
2023-07-28 15:54:06 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+	public class BankAccount
+	{
+		public string Number { get; }
+
+		public string Owner { get; set; }
+
+		public decimal Balance { get; }
+
+		// static as it will be shared by all bankAccount objects
+		private static int accountNumberSeed = 1234567890;
+
+		private List<Transaction> allTransaction = new List<Transaction>();
+
+		//creating a object(constructor) for bank account class to assign those values
+		public BankAccount(string name, decimal intialBalance)
+		{
+			this.Owner = name;
+			this.Balance = intialBalance;
+			this.Number = accountNumberSeed.ToString();
+			accountNumberSeed++;
+
+		}
+
+		public void MakeDeposit(decimal amount, DateTime date, string note)
+		{
+
+		}
+
+		public void MakeWithdrawal(decimal amount, DateTime date, string note)
+		{
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using MySuperBank;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		//creating a bank account object by calling the constructor.
+		var account = new BankAccount("Kendra", 10000);
+		Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+	}
+} 
